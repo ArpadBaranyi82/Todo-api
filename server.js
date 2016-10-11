3104702e3813933fd31c2f3ca9f35b233b174744
Arpad Baranyi <azb4782prgng@gmail.com>
2016-10-11 12:01:24 +0200

    add put

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var todoNextID = 1;
 
 app.use(bodyParser.json());
 
+// GET
+
 app.get('/', function(req, res){
 	res.send("Todo API Root");
 })
@@ -29,6 +31,8 @@ app.get("/todos/:id", function(req,res){
 	}
 	
 });
+
+// POST
 
 app.post("/todos", function(req,res){
 	var body = _.pick(req.body, "description", "completed");
@@ -61,6 +65,37 @@ app.post("/todos", function(req,res){
 		}
 	});
 
+
+// PUT [UPDATE]
+
+		app.put('/todos/:id', function(req,res){
+			var body = _.pick(req.body, "description", "completed");
+			var validAttributes = {};
+			
+			var todoId = parseInt(req.params.id, 10);
+			var matchedTodo = _.findWhere(todos,{id: todoId});
+			
+			if(!matchedTodo){
+				return res.status(404).send();
+			}
+			
+			if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+				validAttributes.completed = body.completed;
+			}else if(body.hasOwnProperty("completed")){
+				return res.status(400).send();
+			}
+
+			if(body.hasOwnProperty("description") && _.isString(body.description) && body.description.trim().length > 0){
+				validAttributes.description = body.description;
+			}else if(body.hasOwnProperty("description")){
+				return res.status(400).send();
+			}
+			
+			matchedTodo = _.extend(matchedTodo, validAttributes);
+			res.json(matchedTodo);
+					
+		})
+	
 
 app.listen(PORT, function(){
 	console.log("Express listening on port " + PORT);
