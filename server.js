ea1e9dabaac78bfd9d6d5f2c62ca8e3e76bddbd6
Arpad Baranyi <azb4782prgng@gmail.com>
2016-10-12 10:43:00 +0200

    convert get to sequelizes

--- server.js
+++ server.js
@@ -38,16 +38,26 @@ app.get('/todos', function(req,res){
 	res.json(filteredTodos);
 });
 
+// SEQUELIZED GET...
 app.get("/todos/:id", function(req,res){
 	var todoId = parseInt(req.params.id,10);
-	var matchedTodo = _.findWhere(todos,{id: todoId});
+	/*var matchedTodo = _.findWhere(todos,{id: todoId});
 	
 	if(matchedTodo){
 		res.json(matchedTodo);
 	}else{
 		res.status(404).send();
-	}
+	}*/
 	
+	db.todo.findById(todoId).then(function(todo){
+		if(!!todo){
+			res.json(todo.toJSON());
+		}else{
+			res.status(404).send();
+		}
+	}, function(e){
+			res.status(500).send();
+	});
 });
 
 // POST...
