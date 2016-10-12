da9641929ef2710b9f71b44ebd5ac0c765017b76
Arpad Baranyi <azb4782prgng@gmail.com>
2016-10-12 10:28:01 +0200

    todo-model

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var _ = require("underscore");
+var db = require("./db.js");
 
 var app = express();
 
@@ -54,17 +55,22 @@ app.get("/todos/:id", function(req,res){
 app.post("/todos", function(req,res){
 	var body = _.pick(req.body, "description", "completed");
 	
-	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+	db.todo.create(body).then(function(todo){
+		res.json(todo.toJSON());
+	}, function(e){
+		res.status(400).json(e);
+	});
+	
+	/*if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
 		return res.status(400).send();
 	}
 	
 	body.description = body.description.trim();
-	
 	body.id = todoNextID;
 	todos.push(body);
 	todoNextID++;
 	
-	res.json(body);
+	res.json(body);*/
 });
 
 
@@ -111,12 +117,20 @@ app.post("/todos", function(req,res){
 			matchedTodo = _.extend(matchedTodo, validAttributes);
 			res.json(matchedTodo);
 					
+		});
+		
+	// DATABASE...
+	
+	db.sequelize.sync().then(function(){
+		app.listen(PORT, function(){
+			console.log("Express listening on port " + PORT + "!");
 		})
+	})
 	
 
-app.listen(PORT, function(){
+/*app.listen(PORT, function(){
 	console.log("Express listening on port " + PORT);
-})
+})*/
 
 
 
